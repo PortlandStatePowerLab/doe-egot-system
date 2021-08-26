967c1f8884fbd792f2e838e31bda930661d456d5
blusky-cloud <sspitzer@pdx.edu>
2021-08-26 21:18:42 +0000

    setup bash script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install -y build-essential wget git pkg-config  cmake python3 python3-dev libssl-dev libmodbus-dev
+mkdir ~/temp
+cd ~/temp
+wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4.tar.gz
+tar -xf cmake-3.18.4.tar.gz
+cd cmake-3.18.4 && cmake . && make && sudo make install
+cd ~/temp
+wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz
+tar -xf boost_1_76_0.tar.gz
+cd boost_1_76_0 && ./bootstrap.sh 
+./b2 && sudo ./b2 install
+cd ~/temp/boost_1_76_0
+./bootstrap.sh --with-python=/usr/bin/python3
+sudo ./b2 --with-python install
+cd ~/temp
+wget https://ftp.wayne.edu/apache//xerces/c/3/sources/xerces-c-3.2.3.tar.gz
+tar -xf xerces-c-3.2.3.tar.gz
+cd xerces-c-3.2.3 && ./configure --prefix=/usr && make && sudo make install
+cd
+rm -rf ~/temp
+sudo apt update -y
+sudo apt upgrade -y
