f90cac0e376247a4dbd4d902cc42ff8a60cb3b60
nicolehenderson <nicole.henderson686@gmail.com>
2022-08-09 10:38:36 -0700

    restructuring of the https client so we have both a singleton and a regular object for different use cases

--- dtm/trust_https/include/trust_https/trust_https.hpp
+++ dtm/trust_https/include/trust_https/trust_https.hpp
@@ -1,37 +1,58 @@
 #ifndef __TRUST_HTTPS_H__
 #define __TRUST_HTTPS_H__
 
-#include <https/https_client.hpp>
+#include <https/abstract_client.hpp>
 
-/**
- * The base Decorator class follows the same interface as the other components.
- * The primary purpose of this class is to define the wrapping interface for all
- * concrete decorators. The default implementation of the wrapping code might
- * include a field for storing a wrapped component and the means to initialize
- * it.
- */
+// Singleton Design Pattern
 class TrustHttps : public AbstractClient
 {
 public:
-    TrustHttps(HttpsClient *https_client, HttpsClient *dtm_client);
-    ~TrustHttps();
+    // rule of 5
+    TrustHttps(TrustHttps &other) = delete;         // clonable
+    void operator=(const TrustHttps &) = delete;    // assignable
 
-    // The Decorator delegates all work to the wrapped component.
+    static TrustHttps *getInstance();
+
+    boost::multiprecision::uint256_t getLFDI ();
+
     boost::beast::http::response<boost::beast::http::dynamic_body> Get(
-        const std::string &target, const std::string &query = "");
+        const std::string &target, const std::string &query = "") override;
 
     boost::beast::http::response<boost::beast::http::dynamic_body> Post(
-        const std::string &target, const std::string &resource);
+        const std::string &target, const std::string &resource) override;
 
     boost::beast::http::response<boost::beast::http::dynamic_body> Put(
-        const std::string &target, const std::string &resource);
+        const std::string &target, const std::string &resource) override;
 
     boost::beast::http::response<boost::beast::http::dynamic_body> Delete(
-        const std::string &target);
+        const std::string &target) override;
 
 protected:
-    HttpsClient *https_client_;
-    HttpsClient *dtm_client_;
+    HttpsClient(const std::string& id, const std::string &root, const std::string &host, const std::string &port);
+    ~HttpsClient() noexcept;
+
+private:
+    void readLFDI (const std::string& root);
+    boost::beast::ssl_stream<boost::beast::tcp_stream> Connect();
+    boost::beast::http::response<boost::beast::http::dynamic_body> Send(
+        boost::beast::http::request<boost::beast::http::string_body> &req);
+
+private:
+    std::string id_;
+    std::string root_;
+    std::string host_;
+    std::string port_;
+    boost::multiprecision::uint256_t lfdi_;
+
+    // required for all boost beast I/O
+    boost::asio::io_context io_context_;
+    boost::asio::ssl::context ssl_context_;
+    boost::asio::ip::tcp::resolver resolver_;
+
+private:
+    // thread-safe singleton instance
+    static HttpsClient *instance_;
+    static std::mutex mutex_;
 };
 
 #endif // __TRUST_HTTPS_H__
