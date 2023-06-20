ea7850d8ded6f99543dead3819c0f04111e8aeea
Tylores <tylor.slay@gmail.com>
2023-06-20 08:02:47 -0700

    updated the ssl gen script to capture registration info for dcm and gsp

--- ssl/cert_gen.sh
+++ ssl/cert_gen.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
+import re
 ctr=1
+pattern='((:.{2}){20}$)'
+rm registration.csv
 while [ $ctr -le $1 ]
 do
-openssl req -new \
-    -config sub-ca.conf \
-    -out root-ca/client$ctr.csr \
-    -keyout root-ca/private/client$ctr.key
+FILE=root-ca/client$ctr.crt
+if [ -f "$FILE" ]; then
+    echo "$FILE exists"
+    fingerprint=$(openssl x509 -in $FILE -noout -fingerprint -sha256)
+    [[ $fingerprint =~ $pattern ]]
+    lfdi="${BASH_REMATCH[0]}"
+    lfdi=${lfdi//:}
+    echo "$ctr,$lfdi" >> registration.csv
+else
+    openssl req -new \
+        -config sub-ca.conf \
+        -out root-ca/client$ctr.csr \
+        -keyout root-ca/private/client$ctr.key
 
-openssl ca -batch\
-    -config sub-ca.conf \
-    -in root-ca/client$ctr.csr \
-    -out root-ca/client$ctr.crt \
-    -extensions client_ext
+    openssl ca -batch\
+        -config sub-ca.conf \
+        -in root-ca/client$ctr.csr \
+        -out root-ca/client$ctr.crt \
+        -extensions client_ext
 
+    fingerprint=$(openssl x509 -in $FILE -noout -fingerprint -sha256)
+    [[ $fingerprint =~ $pattern ]]
+    lfdi="${BASH_REMATCH[0]}"
+    lfdi=${lfdi//:}
+    echo "$ctr,$lfdi" >> registration.csv
+fi
 ((ctr++))
 done
 echo All done
