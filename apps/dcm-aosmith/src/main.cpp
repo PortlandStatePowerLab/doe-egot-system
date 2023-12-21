478840569753b56a953878dcc5633abc3e989f08
Tylores <tylor.slay@gmail.com>
2023-12-20 16:20:47 -0800

    updated dcm-aosmith to use the trust cta2045 device

--- apps/dcm-aosmith/src/main.cpp
+++ apps/dcm-aosmith/src/main.cpp
@@ -1,16 +1,15 @@
-#include <cta2045/device.hpp>
-#include <cta2045/ucm.hpp>
+#include <https/client/client.hpp>
 #include <iostream>
 #include <string>
+#include <trust/cta2045/trust_device.hpp>
+#include <utilities/utilities.hpp>
 
 std::string g_program_path;
 
 int main(int argc, char **argv) {
   std::cout << "Starting DCM for AOSmith water heater\n";
-  cta2045::DeviceInfo info =
-      cta2045::Device::getInstance("ttl/0")->getDeviceInfo();
-  std::cout << "info : " << info.vendor_id << "\n"
-            << info.device_type << "\n"
-            << info.firmware_year << std::endl;
+  g_program_path = psu::utilities::getProgramPath(argv);
+  https::Context dtm_ctx = {"1", g_program_path, "0.0.0.0", "8090"};
+  trust::cta2045Device::getInstance("/ttyAMA0", dtm_ctx);
   return 0;
 }
--- libs/cta2045/src/device.cpp
+++ libs/cta2045/src/device.cpp
@@ -1,4 +1,5 @@
 #include <cta2045/device.hpp>
+#include <iostream>
 
 using namespace cea2045;
 
